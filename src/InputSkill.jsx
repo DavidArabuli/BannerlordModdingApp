9d26be8d26e27934e340382feaa996de37934846
David Arabuli <davidsarabuli@gmail.com>
2024-04-21 14:31:07 +0300

    skills inputs are now generated with array.map help, for the sake of keeping jsx smaller

--- src/InputSkill.jsx
+++ src/InputSkill.jsx
@@ -3,6 +3,7 @@ import weaponsArray from "./itemJsons/weaponsArray.json";
 
 const InputSkill = ({ unit }) => {
   const { _attributes: { id } = {}, skills: { skill } = {} } = unit;
+
   const Athletics = skill?.[0];
   const Riding = skill?.[1];
   const OneHanded = skill?.[2];
@@ -11,14 +12,34 @@ const InputSkill = ({ unit }) => {
   const Bow = skill?.[5];
   const Crossbow = skill?.[6];
   const Throwing = skill?.[7];
+
+  const skillsArray = [
+    Athletics,
+    Riding,
+    OneHanded,
+    TwoHanded,
+    Polearm,
+    Bow,
+    Crossbow,
+    Throwing,
+  ];
   const [weapon, setWeapon] = useState(
     unit.Equipments.EquipmentRoster[0]?.equipment[0]?._attributes?.id
   );
+  const weaponstate = Array.isArray(unit.Equipments.EquipmentRoster)
+    ? unit.Equipments.EquipmentRoster[0]?.equipment[0]?._attributes?.id
+    : unit.Equipments.EquipmentRoster.equipment[0]?._attributes?.id;
+  // const [weapon, setWeapon] = useState(
+  //   unit.Equipments.EquipmentRoster[0]?.equipment[0]?._attributes?.id
+  // );
   const handleWeaponChange = (value) => {
     setWeapon(value);
   };
-  const slot =
-    unit.Equipments.EquipmentRoster[0]?.equipment[0]?._attributes?.slot;
+  const slot = Array.isArray(unit.Equipments.EquipmentRoster)
+    ? unit.Equipments.EquipmentRoster[0]?.equipment[0]?._attributes?.slot
+    : unit.Equipments.EquipmentRoster.equipment[0]?._attributes?.slot;
+
+  // unit.Equipments.EquipmentRoster[0]?.equipment[0]?._attributes?.slot;
 
   useEffect(() => {
     const selectElement = document.getElementById(`selector-${id}`);
@@ -29,79 +50,21 @@ const InputSkill = ({ unit }) => {
 
   return (
     <div className="skillBox">
-      <div className="inputLabel">
-        <label>{Athletics?._attributes?.id} :</label>
-        <input
-          type="text"
-          name={`${Athletics?._attributes?.id}-${id}`}
-          defaultValue={Athletics?._attributes?.value}
-          id={id}
-          dataset-id={id}
-        />
-      </div>
-      <div className="inputLabel">
-        <label>{Riding?._attributes?.id} :</label>
-        <input
-          type="text"
-          name={`${Riding?._attributes?.id}-${id}`}
-          defaultValue={Riding?._attributes?.value}
-          id={id}
-        />
-      </div>
-      <div className="inputLabel">
-        <label>{OneHanded?._attributes?.id} :</label>
-        <input
-          type="text"
-          name={`${OneHanded?._attributes?.id}-${id}`}
-          defaultValue={OneHanded?._attributes?.value}
-          id={id}
-        />
-      </div>
-      <div className="inputLabel">
-        <label>{TwoHanded?._attributes?.id} :</label>
-        <input
-          type="text"
-          name={`${TwoHanded?._attributes?.id}-${id}`}
-          defaultValue={TwoHanded?._attributes?.value}
-          id={id}
-        />
-      </div>
-      <div className="inputLabel">
-        <label>{Polearm?._attributes?.id} :</label>
-        <input
-          type="text"
-          name={`${Polearm?._attributes?.id}-${id}`}
-          defaultValue={Polearm?._attributes?.value}
-          id={id}
-        />
-      </div>
-      <div className="inputLabel">
-        <label>{Bow?._attributes?.id} :</label>
-        <input
-          type="text"
-          name={`${Bow?._attributes?.id}-${id}`}
-          defaultValue={Bow?._attributes?.value}
-          id={id}
-        />
-      </div>
-      <div className="inputLabel">
-        <label>{Crossbow?._attributes?.id} :</label>
-        <input
-          type="text"
-          name={`${Crossbow?._attributes?.id}-${id}`}
-          defaultValue={Crossbow?._attributes?.value}
-          id={id}
-        />
-      </div>
-      <div className="inputLabel">
-        <label>{Throwing?._attributes?.id} :</label>
-        <input
-          type="text"
-          name={`${Throwing?._attributes?.id}-${id}`}
-          defaultValue={Throwing?._attributes?.value}
-          id={id}
-        />
-      </div>
+      {skillsArray.map((skill, index) => {
+        return (
+          <div key={index} id={id} className="inputLabel">
+            <label>{skill?._attributes?.id} :</label>
+            <input
+              type="text"
+              name={`${skill?._attributes?.id}-${id}`}
+              defaultValue={skill?._attributes?.value}
+              id={id}
+              dataset-id={id}
+            />
+          </div>
+        );
+      })}
+
       <div className="itemBlock">
         <label>Current weapon: {weapon} | Choose a new weapon: </label>
         <select
